80827e0a6f7eb37dbab79f9148a306d1253a9760
배민준 <sokk159dps@gmail.com>
2026-02-14 23:09:03 +0900

    [feature] 로그인 되어야 결제할 수 있도록

---
+++ src/main/java/com/payserver/exception/ExpiredTokenException.java
@@ -0,0 +1,7 @@
+package com.payserver.exception;
+
+public class ExpiredTokenException extends RuntimeException {
+    public ExpiredTokenException(String message) {
+        super(message);
+    }
+}
